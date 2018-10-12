d69596cf9bb6e9dd6f75df4bdeadb3b6c0814a84
Murlika <murlika11@yandex.ru>
2018-10-12 07:24:34 +0300

    ruby admin

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-	belongs_to :test
+
 	has_many :answers
 	
 	mount_uploader :image, ImagesUploader
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 	has_many :questions
-	has_many :answers, through: :questions
+	#has_many :answers, through: :questions
 	
 	scope :ordered, -> { order :id }
 
